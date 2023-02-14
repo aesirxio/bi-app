0e5055c0f672ade2e99109ebd4297f3101da688f
huy <huytran.dev.web@gmail.com>
2023-02-14 11:37:26 +0700

    #60: event bi

--- src/containers/Behavior/UTMTracking.jsx
+++ src/containers/Behavior/UTMTracking.jsx
@@ -27,7 +27,7 @@ const UTMTrackingPage = observer(() => {
     <div className="py-4 px-3 h-100 d-flex flex-column">
       <div className="d-flex align-items-center justify-content-between mb-24 flex-wrap">
         <div className="position-relative">
-          <h2 className="text-blue-0 fw-bold mb-8px">{t('txt_behavior')}</h2>
+          <h2 className="text-blue-0 fw-bold mb-8px">{t('txt_menu_utm_tracking')}</h2>
         </div>
         <div className="position-relative">
           <DateRangePicker viewModelArr={[behaviorViewModel]} />

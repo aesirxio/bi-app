0167a584ecd201a8daa75933c095cd60356a7708
Viet <viet@r-digital.tech>
2023-05-29 14:28:29 +0700

    Definition tooltip

--- src/components/Table/index.jsx
+++ src/components/Table/index.jsx
@@ -129,7 +129,7 @@ const Table = ({
                         >
                           {column.render('Header')}
                           {canSort && (
-                            <span className="position-relative">
+                            <span className="position-relative align-middle">
                               {sortAPI ? (
                                 store?.sortBy?.id === sortParams &&
                                 sortParams !== 'number' &&
@@ -146,10 +146,11 @@ const Table = ({
                                     />
                                   )
                                 ) : (
-                                  <ComponentSVG
-                                    url={env.PUBLIC_URL + '/assets/images/sort.svg'}
-                                    color="#5F5E70"
-                                  />
+                                  // <ComponentSVG
+                                  //   url={env.PUBLIC_URL + '/assets/images/sort.svg'}
+                                  //   color="#5F5E70"
+                                  // />
+                                  <></>
                                 )
                               ) : !column.rowSpan ? (
                                 column.isSorted &&
@@ -167,10 +168,11 @@ const Table = ({
                                     />
                                   )
                                 ) : (
-                                  <ComponentSVG
-                                    url={env.PUBLIC_URL + '/assets/images/sort.svg'}
-                                    color="#5F5E70"
-                                  />
+                                  <></>
+                                  // <ComponentSVG
+                                  //   url={env.PUBLIC_URL + '/assets/images/sort.svg'}
+                                  //   color="#5F5E70"
+                                  // />
                                 )
                               ) : columnInside.isSorted &&
                                 // Column have rowSpan

16ae506cfa22e004b1820d227ef8294c263d18ba
trucduy10 <trucduy.truong@gmail.com>
2023-10-26 16:18:52 +0700

    delete defined but never used

---
+++ packages/aesirx-bi-app/src/containers/VisitorsPage/CityModel/CityModel.js
@@ -0,0 +1,98 @@
+/*
+ * @copyright   Copyright (C) 2022 AesirX. All rights reserved.
+ * @license     GNU General Public License version 3, see LICENSE.
+ */
+import React from 'react';
+import { BI_CITIES_FIELD_KEY, BI_SUMMARY_FIELD_KEY, Helper } from 'aesirx-lib';
+import moment from 'moment';
+
+class CityModel {
+  data = [];
+  globalViewModel = null;
+  constructor(entity, globalViewModel) {
+    if (entity) {
+      this.data = entity ?? [];
+      this.globalViewModel = globalViewModel;
+    }
+  }
+
+  toRaw = () => {
+    return this.data;
+  };
+
+  toCitiesTableTop = () => {
+    const headerTable = [
+      'txt_City',
+      'txt_views',
+      'txt_page_views',
+      'txt_unique_page_views',
+      'txt_bounce_rate',
+      'txt_page_session',
+      'txt_time_on_page',
+    ];
+    const accessor = [
+      BI_CITIES_FIELD_KEY.CITY,
+      BI_SUMMARY_FIELD_KEY.NUMBER_OF_VISITORS,
+      BI_SUMMARY_FIELD_KEY.NUMBER_OF_PAGE_VIEWS,
+      BI_SUMMARY_FIELD_KEY.NUMBER_OF_UNIQUE_PAGE_VIEWS,
+      BI_SUMMARY_FIELD_KEY.BOUNCE_RATE,
+      BI_SUMMARY_FIELD_KEY.NUMBER_OF_PAGES_PER_SESSION,
+      BI_SUMMARY_FIELD_KEY.AVERAGE_SESSION_DURATION,
+    ];
+    if (this.data?.length) {
+      const header = accessor.map((key, index) => {
+        return {
+          Header: headerTable[index],
+          accessor: key,
+          width:
+            key === BI_CITIES_FIELD_KEY.CITY
+              ? 250
+              : key === BI_SUMMARY_FIELD_KEY.NUMBER_OF_UNIQUE_PAGE_VIEWS
+              ? 220
+              : 170,
+          Cell: ({ cell, column }) =>
+            column.id === BI_CITIES_FIELD_KEY.CITY ? (
+              <div className={'px-15'}>{cell?.value === '' ? 'Unknown' : cell?.value}</div>
+            ) : column.id === BI_SUMMARY_FIELD_KEY.BOUNCE_RATE ? (
+              <div className={'px-3 text-end'}>{cell?.value + '%' ?? null}</div>
+            ) : column.id === BI_SUMMARY_FIELD_KEY.AVERAGE_SESSION_DURATION ? (
+              <div className={'px-3 text-end'}>
+                {cell?.value ? moment.utc(cell?.value * 1000).format('HH:mm:ss') : '00:00:00'}
+              </div>
+            ) : (
+              <div className={'px-15 text-end'}>{Helper.numberWithCommas(cell?.value) ?? null}</div>
+            ),
+        };
+      });
+      const data = this.data
+        ?.map((item) => {
+          return {
+            ...item,
+            ...accessor
+              .map((i) => {
+                return {
+                  [i]: item[i],
+                };
+              })
+              .reduce((accumulator, currentValue) => ({ ...currentValue, ...accumulator }), {}),
+          };
+        })
+        ?.sort(
+          (a, b) =>
+            b[BI_SUMMARY_FIELD_KEY.NUMBER_OF_VISITORS] - a[BI_SUMMARY_FIELD_KEY.NUMBER_OF_VISITORS]
+        );
+
+      return {
+        header,
+        data: data,
+      };
+    } else {
+      return {
+        header: [],
+        data: [],
+      };
+    }
+  };
+}
+
+export default CityModel;
